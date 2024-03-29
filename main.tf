c6c3907f8f6dd2350efdfc77beea5bd4ae50357d
Nimra Naveed <98296821+nimnaveed@users.noreply.github.com>
2024-03-28 18:08:05 -0700

    final

--- main.tf
+++ main.tf
@@ -34,32 +34,22 @@ module "blog_vpc" {
   }
 }
 
-
-module "autoscaling" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-  version = "7.4.0"
-  # insert the 1 required variable here
-
-  name = "blog"
-  min_size = 1
-  max_size = 2
-
-  vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns
-  security_groups = [module.blog_sg.security_group_id]
-  
-  image_id      = data.aws_ami.app_ami.id
+resource "aws_instance" "blog" {
+  ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
+  subnet_id = module.blog_vpc.public_subnets[0]
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
 
 module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name    = "blog-alb"
-
-  load_balancer_type = "application"
-  
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
@@ -106,6 +96,7 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
+      target_id = aws_instance.blog.id
     }
   }
 
